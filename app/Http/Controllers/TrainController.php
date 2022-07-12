76acf23170e2728b06673b14f21cc4e45ec84ec3
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-07-12 18:14:09 +0200

    sistemato db con orario treni più ristretto

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class TrainController extends Controller
 {
     public function index()
-    {
+    {   
         $trains=Train::where('orario_di_partenza', Carbon::today())->get();
         
         return view('home', compact('trains'));
--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -13,14 +13,14 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for($i = 0; $i < 20; $i++){
+        for($i = 0; $i < 30; $i++){
             $newTrain = new Train();
             //tutte le colonne
             $newTrain->azienda= $faker->company();
             $newTrain->stazione_di_partenza= $faker->city();
             $newTrain->stazione_di_arrivo= $faker->city();
-            $newTrain->orario_di_partenza= $faker->dateTimeThisYear();
-            $newTrain->orario_di_arrivo= $faker->dateTimeThisYear();
+            $newTrain->orario_di_partenza= $faker->dateTimeBetween('2022-07-11 00:01:10','2022-07-13 23:59:10');
+            $newTrain->orario_di_arrivo= $faker->dateTimeBetween('2022-07-11 00:01:10','2022-07-13 23:59:10');
             $newTrain->codice_treno= $faker->randomNumber(5, 20);
             $newTrain->numero_carrozza= $faker->randomDigitNotNull();
             $newTrain->save();
