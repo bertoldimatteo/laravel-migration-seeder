2dbb1f610d3679966ebb551a592ab01e5ef9480d
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-07-12 16:40:53 +0200

    stuck

--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -13,16 +13,17 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        $newTrain = new Train();
-        //tutte le colonne
-        $newTrain->azienda= $faker->company();
-        $newTrain->stazione_di_partenza= $faker->city();
-        $newTrain->stazione_di_arrivo= $faker->city();
-        $newTrain->orario_di_partenza= $faker->time();
-        $newTrain->orario_di_arrivo= $faker->time();
-        $newTrain->codice_treno= $faker->randomNumber(10, true);
-        $newTrain->numero_carrozza= $faker->randomDigitNotNull();
-
-        $newTrain->save();
+        for($i = 0; $i > 20; $i++){
+            $newTrain = new Train();
+            //tutte le colonne
+            $newTrain->azienda= $faker->company();
+            $newTrain->stazione_di_partenza= $faker->city();
+            $newTrain->stazione_di_arrivo= $faker->city();
+            $newTrain->orario_di_partenza= $faker->time();
+            $newTrain->orario_di_arrivo= $faker->time();
+            $newTrain->codice_treno= $faker->randomNumber(10, true);
+            $newTrain->numero_carrozza= $faker->randomDigitNotNull();
+            $newTrain->save();
+        }
     }
 }
