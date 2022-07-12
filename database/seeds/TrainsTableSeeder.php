ddabc7d0e99a4415b636c2f41436936ef3ceae06
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-07-12 16:22:47 +0200

    aggiunto dati fake

--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Train;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -9,8 +11,18 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $newTrain = new Train();
+        //tutte le colonne
+        $newTrain->azienda= $faker->company();
+        $newTrain->stazione_di_partenza= $faker->city();
+        $newTrain->stazione_di_arrivo= $faker->city();
+        $newTrain->orario_di_partenza= $faker->time();
+        $newTrain->orario_di_arrivo= $faker->time();
+        $newTrain->codice_treno= $faker->randomNumber(10, true);
+        $newTrain->numero_carrozza= $faker->randomDigitNotNull();
+
+        $newTrain->save();
     }
 }
